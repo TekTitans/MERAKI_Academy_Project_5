e4c91428790fa9da869b7464b25982ba254ec4f0
AlKhateebKhaled <khaled.hkhateeb@gmail.com>
2024-11-10 21:38:52 +0300

    Save local changes to index.js before pulling

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const db = require("./models/db");
+const userRouter = require("./routes/user");
+const rolesRouter = require("./routes/role");
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Handles any other endpoints [unassigned - endpoints]
+app.use("/users", userRouter); 
+app.use("/roles", rolesRouter); 
+
+
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
 app.listen(PORT, () => {
