ca8d417f0a6b40720975ede894315c38cfb0641c
Tareqizmirli <Tareqizmirli95@gmail.com>
2024-11-10 21:26:41 +0300

    category+subcategory

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,14 @@ const db = require("./models/db");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cateogryRouter = require("./routes/cateogry");
+const subcategoriesRouter = require("./routes/subcategory");
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/category", cateogryRouter);
+app.use("/subcateogry", subcategoriesRouter);
 // Handles any other endpoints [unassigned - endpoints]
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
