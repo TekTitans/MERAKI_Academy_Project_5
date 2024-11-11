0234af6f11094f94f45bb9b91b6ab74263196cf3
Tareqizmirli <Tareqizmirli95@gmail.com>
2024-11-11 16:27:10 +0300

    products

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const cateogryRouter = require("./routes/cateogry");
 const subcategoriesRouter = require("./routes/subcategory");
+const productRouter = require("./routes/products");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/category", cateogryRouter);
 app.use("/subcateogry", subcategoriesRouter);
+app.use("/products", productRouter);
 // Handles any other endpoints [unassigned - endpoints]
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
