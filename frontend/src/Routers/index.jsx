d77280932cef1930b0e766c57a6f81c02cfb45e9
Tareqizmirli <Tareqizmirli95@gmail.com>
2024-11-26 21:26:12 +0300

    sellerid

--- frontend/src/Routers/index.jsx
+++ frontend/src/Routers/index.jsx
@@ -20,6 +20,7 @@ import Chat from "../pages/chat/Chat";
 
 import PlaceOrder from "../pages/place order/PlaceOrder";
 import AdminDashboard from "../pages/AdminDashboard copy";
+import UserProfile from "../pages/UserProfile";
 export const router = createBrowserRouter(
   [
     {
@@ -52,6 +53,7 @@ export const router = createBrowserRouter(
         { path: "/seller", element: <SellerDashboard /> },
         { path: "/chat", element: <Chat /> },
         { path: "/Admin", element: <AdminDashboard /> },
+        { path: "/users/:userId", element: <UserProfile/>}
       ],
     },
   ],
