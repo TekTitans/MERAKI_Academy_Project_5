a86bb3516a42fcecc0d34a1b81dcbc9515104c46
AlKhateebKhaled <khaled.hkhateeb@gmail.com>
2025-01-02 03:50:39 +0300

    deploy

--- frontend/src/Routers/index.jsx
+++ frontend/src/Routers/index.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
+import Home from "../pages/home";
 import NotFound from "../pages/NotFound";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
